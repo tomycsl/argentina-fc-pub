cfeb4979c2f0a2aa04b6573d719832b2026c433d
Tomas Campodonico <tomycsl@gmail.com>
2022-10-04 12:13:50 +0100

    Remove unused code

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -92,7 +92,7 @@ function App() {
       paid: true
     }]
   }]);
-  const [prices, setPrices] = useState<any[]>(Menu);
+  const [prices] = useState<any[]>(Menu);
 
   const updateTab = (newTab: any[]) => {
     setTab(newTab);
